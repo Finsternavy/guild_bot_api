c5589d9fa4ada695a754668d877c85717c97b708
Christopher Finster <finsternavy@gmail.com>
2022-10-16 00:30:34 -0500

    added guild breakdown class stats and tank search

--- server.py
+++ server.py
@@ -9,6 +9,93 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+def update_guild_stats():
+    cursor = database.members.find({})
+            
+    guild_class_stats = {
+        'title': 'guild_stats',
+        'tanks': [],
+        'healers': [],
+        'DPS/Support': [],
+        'true_tanks': 0,
+        'primary_tanks': 0,
+        'augmented_tanks': 0,
+        'true_healers': 0,
+        'primary_healers': 0,
+        'augmented_healers': 0,
+        'true_dps_support': 0,
+        'primary_dps_support': 0,
+        'augmented_dps_support': 0,
+        'total_registered_forces': 0
+    }
+
+
+    for member in cursor:
+        print(member)
+        print("line 78")
+        name_and_class = str(member['username']) + ' : ' + str(member['class'])
+        
+        
+        # Compile stats based on current members of the guilds class and augments
+        if str(member['primary_role']) == str(member['secondary_role']):
+            if str(member['primary_role']) == 'tank':
+                guild_class_stats['tanks'].append(name_and_class + ' (True Tank)')
+                guild_class_stats['true_tanks'] += 1
+                
+            elif str(member['primary_role']) == 'healer':
+                guild_class_stats['healers'].append(name_and_class + ' (True Healer)')
+                guild_class_stats['true_healers'] += 1
+                
+            else:
+                guild_class_stats['DPS/Support'].append(name_and_class + ' (True DPS / Support)')
+                guild_class_stats['true_dps_support'] += 1
+        
+        else:
+            if str(member['primary_role']) == 'tank':
+                guild_class_stats['tanks'].append(name_and_class + ' (Primary Tank)')
+                guild_class_stats['primary_tanks'] += 1
+                
+                if str(member['secondary_role']) == 'healer':
+                    guild_class_stats['healers'].append(name_and_class + ' (Augmented Healer)')
+                    guild_class_stats['augmented_healers'] += 1
+                else:
+                    guild_class_stats['DPS/Support'].append(name_and_class + ' (Primary DPS/Support)')
+                    guild_class_stats['primary_dps_support'] += 1
+                
+            elif str(member['primary_role']) == 'healer':
+                guild_class_stats['healers'].append(name_and_class + ' (Primary Healer)')
+                guild_class_stats['primary_healers'] += 1
+                
+                if str(member['secondary_role']) == 'tank':
+                    guild_class_stats['tanks'].append(name_and_class + ' (Augmented Tank)')
+                    guild_class_stats['augmented_tanks'] += 1
+                else:
+                    guild_class_stats['DPS/Support'].append(name_and_class + ' (Primary DPS/Support)')
+                    guild_class_stats['primary_dps_support'] += 1
+                
+            elif str(member['primary_role']) == 'DPS/Support':
+                guild_class_stats['DPS/Support'].append(name_and_class + ' (Primary DPS/Support)')
+                guild_class_stats['primary_dps_support'] += 1
+                
+                if str(member['secondary_role']) == 'tank':
+                    guild_class_stats['DPS/Support'].append(name_and_class + ' (Augmented Tank')
+                    guild_class_stats['augmented_tanks'] += 1
+                    
+                else:
+                    guild_class_stats['healers'].append(name_and_class + ' (Augmented Healer)')
+                    guild_class_stats['augmented_healers'] += 1
+                                
+                                
+        guild_class_stats['total_registered_forces'] += 1
+        
+        # update database
+        old_stats = database.guild_stats.find_one({'title': 'guild_stats'})
+        
+        if not old_stats:
+            database.guild_stats.insert_one(guild_class_stats)
+        else:
+            database.guild_stats.find_one_and_replace({'title': 'guild_stats'}, guild_class_stats)
+
 
 @app.post("/api/update/members")
 def update_members():
@@ -30,34 +117,70 @@ def update_members():
                 print("building database member list...")
                 for member in cursor:
                     current_db_usernames.append(str(member['username']).lower())
+                    print('line 33:')
+                    print(member['username'])
                     
             print('building member list...')
             # build a list of usernames from the current members list received from discord
             for member in member_list:
                 current_users.append(str(member['username']).lower())
-                    
+            
+            # delete old members that are no longer in the guild
             if current_db_usernames:
                 for member in current_db_usernames:
-                    if member not in current_users:
+                    if str(member) not in current_users:
                         database.members.find_one_and_delete({'username' : str(member)})
                     
-            
             # add new member if not present in the list
             for member in member_list:
-                if str(member) not in current_db_usernames:
+                if str(member['username']) not in current_db_usernames:
                     database.members.insert_one(member)
                     print(member)
                 
             print(member_list)
+            
+            update_guild_stats()
                 
             return json.dumps("Updates completed")
+        
         except Exception as e:
             print(e)
             
     return json.dumps("Member data did not load properly.  Did you trying turning if off and back on again?")
+
+
         
+@app.get("/api/guild/class-breakdown")
+def get_guild_class_breakdown():
+    
+    cursor = database.guild_stats.find({})
+    guild_stats = {}
+    
+    # get the current guild stats and adjust the ID for passing via json
+    for stats in cursor:
+        stats['_id'] = str(stats['_id'])
+        guild_stats = stats
+
+    return json.dumps(guild_stats)
+    
+
+
+@app.get("/api/guild/tanks")
+def get_tanks():
+    
+    cursor = database.guild_stats.find({})
+    
+    tanks = {}
+    
+    for stats in cursor:
+        stats['_id'] = str(stats['_id'])
+        tanks = stats['tanks']
+    
+    return json.dumps(tanks)
+
+
         
-@app.post("/api/update/member")
+@app.post("/api/set-user-class")
 def update_member():
     
     classes = ['fighter', 'tank', 'rogue', 'ranger', 'mage', 'summoner', 'cleric', 'bard']
@@ -95,10 +218,32 @@ def update_member():
         return_data = ''
         # set the users class, primary and augment data
         if user_from_db:
-            updated_user = database.members.find_one_and_update({'username' : user_data['username']}, {'$set': {'class': str(user_data['class']), 'primary': str(classes[int(primary_index)]), 'augment': str(classes[int(augment_index)])}})
+            primary = str(classes[int(primary_index)])
+            augment = str(classes[int(augment_index)])
+            primary_role = ''
+            secondary_role = ''
+            if primary == 'tank':
+                primary_role = 'tank'
+            elif primary == 'cleric':
+                primary_role = 'healer'
+            else:
+                primary_role = 'DPS/Support'
+            
+            if augment == 'tank':
+                secondary_role = 'tank'
+            elif augment == 'cleric':
+                secondary_role = 'healer'
+            else:
+                secondary_role = 'DPS/Support'
+                
+            updated_user = database.members.find_one_and_update({'username' : user_data['username']}, {'$set': {'class': str(user_data['class']), 'primary': str(primary), 'augment': str(augment), 'primary_role': str(primary_role), 'secondary_role': str(secondary_role)}})
+            
+            print(updated_user['primary_role'])
             
             # format data to be returned to discord for testing purposes
             return_data = 'Success! Your class has been updated to ' + str(user_data['class']).upper() + '.'
+        
+        update_guild_stats()
         
         return json.dumps(return_data)
         
@@ -109,11 +254,37 @@ def update_member():
 def get_user_class(username):
     
     user = database.members.find_one({'username': str(username)})
-    user['_id'] = str(user['_id'])
     
-    response_string = user['username'] + " - Class: " + str(user['class']).upper() + " (Primary: " + str(user['primary']).upper() + ', Augment: ' + str(user['augment']).upper() + ')'
+    response_string = ''
     
-    print(user)
-    return json.dumps(response_string)
+    if not user:
+        response_string = "That user does not exist.  Please check the spelling and try again."
+        return json.dumps(response_string)
+        
+    if user['class']:
+        response_string = user['username'] + " - Class: " + str(user['class']).upper() + " (Primary: " + str(user['primary']).upper() + ', Augment: ' + str(user['augment']).upper() + ')'
+        return json.dumps(response_string)
+    
+    else:
+        username = str(user['username'])
+        response_string = username + " has not set a class yet."
+        return json.dumps(response_string)
+    
+
+@app.post('/api/clear-user-class/<username>')
+def clear_user_class(username):
+    
+    user = database.members.find_one({'username': str(username)})
+    
+    if not user:
+        response_string = 'User does not exist.  Please check the spelling and try again.'
+        
+    if user['class']:
+        user['class'] = ''
+        user['primary'] = ''
+        user['augment'] = ''
+        database.members.find_one_and_update({'username': str(username)}, {'$set': {'class': '', 'primary': '', 'augment': ''}})
+        response_string = 'Class cleared successfully!'
+        return json.dumps(response_string)
 
 app.run(debug=True)
